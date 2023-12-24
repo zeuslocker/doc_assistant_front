769bdd07b7e9bad2c9a18a96cbb434418a987600
Volodymyr Lapan <volodymyr.lapan@wework.com>
2023-12-24 16:21:44 +0100

    loader

--- src/pages/result.js
+++ src/pages/result.js
@@ -1,7 +1,8 @@
 import * as React from "react"
 import { gql, useQuery } from '@apollo/client';
 import Layout from "../components/layout"
-import { wrapper } from './result.module.css'
+import { HashLoader } from 'react-spinners'
+import { wrapper, loaderText } from './result.module.css'
 const GET_DIAGNOSE_QUERY = gql`
   query getDiagnose($symptoms: String!, $medicalCard: File, $medicalTests: File) {
       getDiagnose(symptoms: $symptoms, medicalCard: $medicalCard, medicalTests: $medicalTests)
@@ -17,22 +18,9 @@ const ResultPage = (props) => {
   return <div className={wrapper}>
     {
       loading ?
-        <div className="flex justify-center items-center h-full">
-          <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-            <div className="animate-pulse flex space-x-4">
-              <div className="rounded-full bg-slate-700 h-10 w-10"></div>
-              <div className="flex-1 space-y-6 py-1">
-                <div className="h-2 bg-slate-700 rounded"></div>
-                <div className="space-y-3">
-                  <div className="grid grid-cols-3 gap-4">
-                    <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                    <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                  </div>
-                  <div className="h-2 bg-slate-700 rounded"></div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="flex flex-col justify-center items-center h-screen">
+          <div><HashLoader color="#36d7b7" /></div>
+          <h2 className={`${loaderText} mt-5`}>Usually take 15-30 seconds</h2>
         </div>
         :
         <div className='relative max-w-5xl mx-auto p-10 not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25'>
